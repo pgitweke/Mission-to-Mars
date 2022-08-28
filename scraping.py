d9ad86801d54e71d29d8b0495afe3ad99ec61b4d
Peter Marwa Gitwekere <petermarwa058@gmail.com>
2022-08-27 21:40:35 -0400

    Adding final project

--- scraping.py
+++ scraping.py
@@ -19,6 +19,7 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
+        "hemispheres": hemispheres(browser),
         "last_modified": dt.datetime.now()
     }
 
@@ -96,6 +97,90 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+def hemispheres(browser):
+   url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+   browser.visit(url)
+   # Click the link, find the sample anchor, return the href
+   hemisphere_image_urls = []
+   for i in range(4):
+       # Find the elements on each loop to avoid a stale element exception
+       browser.find_by_css("a.product-item img")[i].click()
+       hemi_data = scrape_hemisphere(browser.html)
+       hemi_data['img_url'] = hemi_data['img_url']
+       # Append hemisphere object to list
+       hemisphere_image_urls.append(hemi_data)
+       # Finally, we navigate backwards
+       browser.back()
+   return hemisphere_image_urls
+
+def scrape_hemisphere(html_text):
+   # parse html text
+   hemi_soup = soup(html_text, "html.parser")
+   # adding try/except for error handling
+   try:
+       title_elem = hemi_soup.find("h2", class_="title").get_text()
+       sample_elem = hemi_soup.find("a", text="Sample").get("href")
+   except AttributeError:
+       # Image error will return None, for better front-end handling
+       title_elem = None
+       sample_elem = None
+   hemispheres = {
+       "title": title_elem,
+       "img_url": sample_elem
+   }
+   return hemispheres
+
+
+# def hemispheres(browser):
+    
+#     # Visit URL
+#     url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+#     browser.visit(url)
+
+#     # Parse the resulting html with soup
+#     hemisphere_html = browser.html
+#     hemisphere_soup = soup(hemisphere_html, 'html.parser')
+
+#     # Retrieve all items for hemispheres information
+#     items = hemisphere_soup.find_all('div', class_='item')
+
+#     # 2. Create a list to hold the images and titles.
+#     hemisphere_image_urls = []
+
+#     # 3. Write code to retrieve the image urls and titles for each hemisphere.
+
+#     main_url = "https://astrogeology.usgs.gov/"
+
+#     # Create loop to scrape through all hemisphere information
+#     for i in items:
+#         hemisphere = {}
+#         titles = i.find('h3').text
+        
+#         # create link for full image
+#         link_ref = i.find('a', class_='itemLink product-item')['href']
+        
+#         # Use the base URL to create an absolute URL and browser visit
+#         browser.visit(main_url + link_ref)
+        
+#         # parse the data
+#         image_html = browser.html
+#         image_soup = soup(image_html, 'html.parser')
+#         download = image_soup.find('div', class_= 'downloads')
+#         img_url = download.find('a')['href']
+        
+#         print(titles)
+#         print(img_url)
+        
+#         # append list
+#         hemisphere['img_url'] = img_url
+#         hemisphere['title'] = titles
+#         hemisphere_image_urls.append(hemisphere)
+#         browser.back()
+        
+#     # 4. Print the list that holds the dictionary of each image url and title.
+#     return hemisphere_image_urls
+
 
 if __name__ == "__main__":
 
